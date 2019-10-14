b82500cc1c76612db5ce892b5ad1565e551065fb
Lukebro-10 <56080719+Lukebro-10@users.noreply.github.com>
2019-10-14 16:04:40 -0500

    Dynamic navbar, pt 1
    added code in the navbar script to check what section of pages is selection and console log a color. Next step: apply the color to the document.

--- late-antiquity/src/app/containers/default-layout/default-layout.component.ts
+++ late-antiquity/src/app/containers/default-layout/default-layout.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnDestroy, Inject } from '@angular/core';
+import { Component, OnDestroy, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { navItems } from '../../_nav';
+import { Router, NavigationEnd } from '@angular/router'
 
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './default-layout.component.html'
 })
-export class DefaultLayoutComponent implements OnDestroy {
+export class DefaultLayoutComponent implements OnDestroy implements OnInit{
   public navItems = navItems;
   public sidebarMinimized = true;
   private changes: MutationObserver;
   public element: HTMLElement;
-  constructor(@Inject(DOCUMENT) _document?: any) {
+  constructor(@Inject(DOCUMENT) _document?: any, private router: Router) {
 
     this.changes = new MutationObserver((mutations) => {
       this.sidebarMinimized = _document.body.classList.contains('sidebar-minimized');
@@ -22,6 +23,23 @@ export class DefaultLayoutComponent implements OnDestroy {
       attributes: true,
       attributeFilter: ['class']
     });
+
+    //Change Navbar styling based upon page section selected.
+    router.events.subscribe(e => {
+          if(e instanceof NavigationEnd) {
+            if(this.router.url === '/' || this.router.url === '/dashboard') {
+              console.log("I am home")
+            }else if(this.router.url.includes('/rome_west')) {
+              console.log("I am red")
+            }else if(this.router.url.includes('/rome_east')) {
+              console.log("I am purple")
+            }else if(this.router.url.includes('/rome_old')) {
+              console.log("I am gold")
+            }else {
+              console.log("wrong")
+            }
+          }
+        });
   }
 
   ngOnDestroy(): void {
